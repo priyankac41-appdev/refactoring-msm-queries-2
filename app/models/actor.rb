b9bb97e918d5111854ef45891942cbf62133b274
priyankac41 <pchandr1@chicagobooth.edu>
2023-02-15 21:38:15 +0000

    done

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -11,24 +11,27 @@
 #  updated_at :datetime         not null
 #
 class Actor < ApplicationRecord
-  def characters
-    key = self.id
+  # def characters
+  #   key = self.id
 
-    the_many = Character.where({ :actor_id => key })
+  #   the_many = Character.where({ :actor_id => key })
 
-    return the_many
-  end
+  #   return the_many
+  # end
 
-  def filmography
-    the_many = Array.new
+  has_many(:characters, {:class_name => "Character"})
+  has_many(:filmography, {:class_name => "Movie",:through => :characters, :source => :movie})
 
-    self.characters.each do |joining_record|
-      destination_record = joining_record.movie
+  # def filmography
+  #   the_many = Array.new
 
-      the_many.push(destination_record)
-    end
+  #   self.characters.each do |joining_record|
+  #     destination_record = joining_record.movie
 
-    return the_many
-  end
+  #     the_many.push(destination_record)
+  #   end
+
+  #   return the_many
+  # end
 
 end
